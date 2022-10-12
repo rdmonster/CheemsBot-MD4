63671f6cb7dc00b040cee177c77471139b8d2f66
rdmonster <115408779+rdmonster@users.noreply.github.com>
2022-10-12 16:29:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94703534578'] //ur owner number
+global.ownername = "🥶 RD MONSTER 🥶" //ur owner name
+global.ytname = "YT: 😈RD GHOST 😈" //ur yt chanel name
+global.socialm = "GitHub: rdghostmonster" //ur github or insta name
+global.location = "Sri Lanka,Mathara,Hakmana" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['94703534578']
+global.ownertag = '94703534578' //ur tag number
+global.botname = '🥶 RD GHOST BOT 🥶' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
+global.reactmoji = "👻" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "🥶 RD GHOST 🥶✔ " //ur sticker watermark packname
+global.author = "🥶RD GHOST 🥶" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
